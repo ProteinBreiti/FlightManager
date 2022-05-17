cb0a0fbf8f1911864833ddc6987ab0620f4be6b7
ProteinBreiti <alexander.breitenberger@gmx.at>
2022-05-17 14:48:31 +0200

    Does stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+import { FlightModule } from './flight/flight.module';
+import { HttpClientModule } from "@angular/common/http";
+import { RouterModule } from "@angular/router";
+import { FLIGHT_ROUTES } from "./flight/flight.routes";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([...FLIGHT_ROUTES]),
+    HttpClientModule,
+    FlightModule],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
